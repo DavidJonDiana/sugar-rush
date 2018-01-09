fb887d6bf7480bb7348752e2b6fe324c035c998d
David Sehl <davidjsehl@gmail.com>
2018-01-09 15:24:29 -0500

    added product model

---
+++ server/db/models/product.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Product = db.define('product', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT
+    },
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://www.fillmurray.com/200/300',
+        validate: {
+            isUrl: true
+        }
+    },
+    inventoryQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    category: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+
+
+module.exports = Product;
