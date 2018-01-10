c8ea9874a2c2fe3f86d6228be2276ee421b8eb15
DianaLease <DianaRLease@gmail.com>
2018-01-10 11:21:18 -0500

    added product list component

--- client/components/product-list.js
+++ client/components/product-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProductListItem from './product-list-item';
+import { Card } from 'semantic-ui-react'
 
 const dummyproducts = [{
   name: 'dummy',
@@ -7,15 +8,26 @@ const dummyproducts = [{
   description: 'a great candy',
   id: 1,
   price: 10.00
-}]
+},
+  {
+    name: 'data',
+    imageUrl: 'http://www.fillmurray.com/200/250',
+    description: 'yum',
+    id: 2,
+    price: 10.00
+  }]
 
 
 const ProductList = (props) => {
   return (
     <div>
-      <h3>Products</h3>
-      {dummyproducts.map(product => <ProductListItem product={product} key={product.id} />
-      )}
+      <div className="title">
+        <h3>Products</h3>
+      </div>
+     <Card.Group>
+        {dummyproducts.map(product => <ProductListItem product={product} key={product.id} />
+        )}
+      </Card.Group>
     </div>
   )
 }
