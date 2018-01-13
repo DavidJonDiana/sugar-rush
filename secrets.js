c861833cfbed2428709ad72e75d9faf69035d8ec
Jon Rea <jonarea@gmail.com>
2018-01-13 12:18:58 -0500

    can sign in with google

--- secrets.js
+++ secrets.js
@@ -2,4 +2,4 @@ process.env.GOOGLE_CLIENT_ID = '900187317988-jmq191jljfhhottukok4839uit1lfjcm.ap
 
 process.env.GOOGLE_CLIENT_SECRET = 'PsHU1OoarGMtSNogT6VhbvQu'
 
-process.env.GOOGLE_CALLBACK = 'http://localhost:8080/google/callback'
+process.env.GOOGLE_CALLBACK = 'http://localhost:8080/auth/google/callback'
