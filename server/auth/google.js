67da8ec9e700a93066fd70e4685b7d093fe72dde
Jon Rea <jonarea@gmail.com>
2018-01-13 11:58:40 -0500

    google not working yet

--- server/auth/google.js
+++ server/auth/google.js
@@ -34,6 +34,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
     const googleId = profile.id
     const firstName = profile.displayName.split(' ')[0]
     const lastName = profile.displayName.split(' ')[1]
+    console.log(firstName, lastName)
     const email = profile.emails[0].value
 
     User.find({where: {googleId}})
