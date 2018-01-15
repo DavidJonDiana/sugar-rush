d640b28a963b8e89d71e9fa1e2701b8aec809c23
Jon Rea <jonarea@gmail.com>
2018-01-15 13:15:27 -0500

    fix: use - in order-model comp

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import OrderModal from 'order-modal'
 
 /**
  * COMPONENT
@@ -11,6 +12,7 @@ export const UserHome = (props) => {
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <OrderModal />
     </div>
   )
 }
