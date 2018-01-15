d11e5107930e546f0aab9e8b815bf8453f9dbc83
Jon Rea <jonarea@gmail.com>
2018-01-15 13:11:58 -0500

    fix: changed order button text

--- client/components/orderModal.js
+++ client/components/orderModal.js
@@ -3,7 +3,7 @@ import { Button, Modal } from 'semantic-ui-react'
 import OrderForm from 'order-form'
 
 const OrderFormModal = () => (
-  <Modal trigger={<Button>Show Modal</Button>}>
+  <Modal trigger={<Button>Order</Button>}>
     <Modal.Header>Order</Modal.Header>
     <Modal.Content><OrderForm /></Modal.Content>
   </Modal>
