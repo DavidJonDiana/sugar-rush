2d6ec87f156f97db6ce77febca0ff3d417c01d92
Jon Rea <jonarea@gmail.com>
2018-01-16 16:16:19 -0500

    Update auth-form.js

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import { Form } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
 
